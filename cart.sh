44aa63bd1828859efa83e0e09ed3ba415d226e75
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-11-27 15:13:32 -0500

    cart verified

--- cart.sh
+++ cart.sh
@@ -1,52 +1,82 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M)
-LOG_FILE="/tmp/$0_$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-if [ $ID -ne 0 ]
-	then
-		echo "Please try with root access"
-		exit 1
-	else
-		echo "Root User"
-fi
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
+echo "script started executing at $TIMESTAMP" &>> $LOG_FILE
 
-VALIDATE() {
-	echo "validating $PACKAGE"
-if [ $1 -ne 0 ]
-	then
-		echo "FAILED $2" 
-	else
-		echo "SUCCESS $2"
-fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
 
-dnf install nodejs -y
 
-useradd roboshop
+dnf module disable nodejs -y &>> $LOG_FILE
 
-mkdir /app
+VALIDATE $? "Disabling Current NodeJs" 
 
-curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
+dnf module enable nodejs:18 -y &>> $LOG_FILE
 
-cd /app 
+VALIDATE $? "Enabling NodeJs 18" 
 
-unzip /tmp/cart.zip
+dnf install nodejs -y &>> $LOG_FILE
 
-unzip /tmp/cart.zip
+VALIDATE $? "Installing Node Js 18" 
 
-npm install 
+useradd roboshop &>> $LOG_FILE
 
-vim /etc/systemd/system/cart.service
+VALIDATE $? "Creating Roboshop User" 
 
-systemctl daemon-reload
+mkdir /app &>> $LOG_FILE
 
-systemctl enable cart 
+VALIDATE $? "Creating AppDirectory" 
 
-systemctl start cart
+curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip  &>> $LOG_FILE
+
+VALIDATE $? "downloading cart application" 
+
+cd /app  &>> $LOG_FILE
+
+unzip /tmp/cart.zip  &>> $LOG_FILE
+
+VALIDATE $? "unzipping cart application" 
+
+npm install  &>> $LOG_FILE
+
+VALIDATE $? "installing dependencies" 
+
+cp /home/centos/RoboshopShellAbhi/cart.service /etc/systemd/system/cart.service  &>> $LOG_FILE
+
+VALIDATE $? "copied dependenservce" 
+
+systemctl daemon-reload  &>> $LOG_FILE
+
+VALIDATE $? "daemon reloaded" 
+
+systemctl enable cart   &>> $LOG_FILE
+
+VALIDATE $? "enabled cart" 
+
+systemctl start cart  &>> $LOG_FILE
+
+VALIDATE $? "started  cart" 
