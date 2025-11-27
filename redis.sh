ea53e7405c82d9e185d036488ae290aad319c6d9
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-11-27 12:53:11 -0500

    redit verified

--- redis.sh
+++ redis.sh
@@ -30,7 +30,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOG_FILE
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.9.rpm -y &>> $LOG_FILE
 
 VALIDATE $? "installing remi release"
 
