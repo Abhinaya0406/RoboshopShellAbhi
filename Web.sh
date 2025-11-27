0d0b9ca4444fc881c9f88bef37bc9ca8dc87797a
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-11-27 15:22:55 -0500

    web verified

--- Web.sh
+++ Web.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M)
-LOG_FILE="/tmp/$0_$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOG_FILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
-	then
-		echo "Please try with root access"
-		exit 1
-	else
-		echo "Root User"
-fi
-
-
-VALIDATE() {
-	echo "validating $PACKAGE"
-if [ $1 -ne 0 ]
-	then
-		echo "FAILED $2" 
-	else
-		echo "SUCCESS $2"
-fi
-}
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 dnf install nginx -y &>> $LOG_FILE
 
